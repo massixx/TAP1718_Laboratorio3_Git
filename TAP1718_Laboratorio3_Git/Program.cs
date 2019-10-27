7cd267115eb2b5203ceac6c8107e069208aa62cd
massixx <sini.massimiliano@gmail.com>
2019-10-27 20:18:53 +0100

    commit dopo fetch  e pull

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -7,6 +7,7 @@ namespace TAP1718_Laboratorio3_Git
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("ho fatto la fetch e pull dopo aver annullato un paio di commit e ora se mi vedi cosi è perchè ho fatto una nuova commit e push sul server :) !");
+        }    
     }
 }
