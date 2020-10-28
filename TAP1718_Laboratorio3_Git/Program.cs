44d0fd54e40e1e44aa9e2714beaa273f2898323e
massixx <sini.massimiliano@gmail.com>
2020-10-28 14:44:40 +0100

    aggiunta riga in program.cs,ora faccio push

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -8,6 +8,7 @@ namespace TAP1718_Laboratorio3_Git
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("ho fatto la fetch e pull dopo aver annullato un paio di commit e ora se mi vedi cosi è perchè ho fatto una nuova commit e push sul server :) !");
+            Console.WriteLine("modifica dopo pull");
         }    
     }
 }
